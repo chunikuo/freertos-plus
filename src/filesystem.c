cfdb14b21a32f2d8770ded599bf469b5c15dca72
Chun-I Kuo <chuni.kuo@gmail.com>
2014-10-03 01:17:06 +0800

    Add: fs_show fs_show can will call the callback function of romfs_show It will show all filenames in the targeted filesystem

--- src/filesystem.c
+++ src/filesystem.c
@@ -1,6 +1,7 @@
 #include "osdebug.h"
 #include "filesystem.h"
 #include "fio.h"
+#include "clib.h"
 
 #include <stdint.h>
 #include <string.h>
@@ -10,6 +11,7 @@
 
 struct fs_t {
     uint32_t hash;
+    fs_show_t show;
     fs_open_t cb;
     void * opaque;
 };
@@ -20,13 +22,14 @@ __attribute__((constructor)) void fs_init() {
     memset(fss, 0, sizeof(fss));
 }
 
-int register_fs(const char * mountpoint, fs_open_t callback, void * opaque) {
+int register_fs(const char * mountpoint, fs_open_t callback, fs_show_t showcallback, void * opaque) {
     int i;
     DBGOUT("register_fs(\"%s\", %p, %p)\r\n", mountpoint, callback, opaque);
     
     for (i = 0; i < MAX_FS; i++) {
         if (!fss[i].cb) {
             fss[i].hash = hash_djb2((const uint8_t *) mountpoint, -1);
+	    fss[i].show = showcallback;
             fss[i].cb = callback;
             fss[i].opaque = opaque;
             return 0;
@@ -60,3 +63,28 @@ int fs_open(const char * path, int flags, int mode) {
     
     return -2;
 }
+
+
+int fs_show(const char * path)
+{
+    const char * slash;
+    uint32_t hash;
+    int i;
+    while (path[0] == '/')
+	path++;
+
+    slash = strchr(path, '/');
+    if (!slash)
+	return -2;
+
+    hash = hash_djb2((const uint8_t *) path, slash - path);
+    path = slash + 1;
+
+    for (i = 0; i < MAX_FS; ++i) {
+	if (fss[i].hash == hash)
+	   return fss[i].show(fss[i].opaque, path); 
+    }
+
+    return -2;	
+}
+
